6cb07783c839cbaf6ac1789665fdf0ac39ad8ba6
Catacar2006 <catalinacardenas459@gmail.com>
2024-08-06 09:49:38 -1200

    commit inicial

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>DANIEL FERNANDO RAMÍREZ LASSO</Text>
+    </View>
+  );
+}
